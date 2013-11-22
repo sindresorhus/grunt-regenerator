1bd27efdbd4761b86c41bcd4d096d566269c5add
Sindre Sorhus <sindresorhus@gmail.com>
2013-11-22 18:40:11 +0100

    Remove deprecated grunt util

--- tasks/regenerator.js
+++ tasks/regenerator.js
@@ -1,10 +1,12 @@
 'use strict';
 var regenerator = require('regenerator');
+var async = require('async');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('regenerator', 'Transpile ES6 generator functions to ES5', function () {
 		var options = this.options();
-		grunt.util.async.forEach(this.files, function (el, next) {
+
+		async.each(this.files, function (el, next) {
 			var js = el.src.map(function (filePath) {
 				return grunt.file.read(filePath);
 			}).join(grunt.util.linefeed);
