882f7758ccfc22e36e23b79eb0e78dbf1ab3a602
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-25 22:30:53 +0100

    use each-async and drop concatenating
    use grunt-contrib-concat for concat

--- tasks/regenerator.js
+++ tasks/regenerator.js
@@ -1,17 +1,13 @@
 'use strict';
+var eachAsync = require('each-async');
 var regenerator = require('regenerator');
-var async = require('async');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('regenerator', 'Transpile ES6 generator functions to ES5', function () {
 		var options = this.options();
 
-		async.each(this.files, function (el, next) {
-			var js = el.src.map(function (filePath) {
-				return grunt.file.read(filePath);
-			}).join(grunt.util.linefeed);
-
-			grunt.file.write(el.dest, regenerator(js, options));
+		eachAsync(this.files, function (el, i, next) {
+			grunt.file.write(el.dest, regenerator(grunt.file.read(el.src[0]), options));
 			next();
 		}, this.async());
 	});
